f63a02847f437d89834d7e282fd6745526959816
wodomierz <miron.ficak@gmail.com>
2020-10-04 12:59:12 +0200

    done

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -9,21 +9,29 @@ using Newtonsoft.Json.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class BaseGradeBook
+    public abstract class BaseGradeBook
+
     {
         public string Name { get; set; }
+        public GradeBookType Type { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
+
         }
 
         public void AddStudent(Student student)
         {
-            if (string.IsNullOrEmpty(student.Name))
+            if (string.IsNullOrEmpty(student.Name)) 
+            {
                 throw new ArgumentException("A Name is required to add a student to a gradebook.");
+            }
+            
             Students.Add(student);
         }
 
@@ -32,6 +40,8 @@ namespace GradeBook.GradeBooks
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentException("A Name is required to remove a student from a gradebook.");
             var student = Students.FirstOrDefault(e => e.Name == name);
+            
+
             if (student == null)
             {
                 Console.WriteLine("Student {0} was not found, try again.", name);
@@ -106,20 +116,34 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int result = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    result = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    result = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    result = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    result = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    result = 0;
+                    break;
             }
-            return 0;
+
+            if (studentType == StudentType.Honors || studentType == StudentType.DualEnrolled) {
+                if (IsWeighted) { 
+
+                result += 1;
+                }
+            }
+            return result;
+
         }
 
         public virtual void CalculateStatistics()
---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,88 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+
+    {
+
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted)
+        {
+
+            Type = GradeBookType.Ranked;
+
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            var n = Students.Count;
+
+            if (n < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            int counter = 0;
+            foreach (Student s in this.Students)
+            {
+                foreach (double grade in s.Grades)
+                {
+                    if (averageGrade < grade)
+                    {
+                        counter += 1;
+                    }
+                }
+            }
+            int betterPercent = counter * 100 / n;
+
+            if (betterPercent < 20)
+            {
+                return 'A';
+            }
+            if (betterPercent < 40)
+            {
+                return 'B';
+            }
+            if (betterPercent < 60)
+            {
+                return 'C';
+            }
+            if (betterPercent < 80)
+            {
+                return 'D';
+            }
+
+            return 'F';
+
+
+
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+
+            }
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {   
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else { 
+            base.CalculateStudentStatistics(name);
+        }
+        }
+    }
+}
